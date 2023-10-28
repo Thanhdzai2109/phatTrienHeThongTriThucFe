fe01fb0058ed0a75115edb536f34fb3ed53181ad
loitran299 <loitran299@gmail.com>
2023-10-28 22:27:31 +0700

    mode edit user profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
 import {
   DefaultFooterComponent,
   DefaultHeaderComponent,
@@ -96,7 +97,8 @@ const APP_CONTAINERS = [
     CertificateModule,
     ModalModule,
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     {
